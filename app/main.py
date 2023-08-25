33c7e1f8bb29403d08a312a9c36f34064c701474
nhjeon <nakhyun@jobis.co>
2023-08-25 19:03:22 +0900

    test

--- app/main.py
+++ app/main.py
@@ -1,3 +1,27 @@
 import os
+from typing import List
+
+# Authentication is defined via github.Auth
+from github import Auth
+from github import Github
+
+# using an access token
+auth = Auth.Token("access_token")
+
+g = Github(auth=auth)
+g = Github(base_url="https://github.com/api/v3", auth=auth)
+
+# Then play with your Github objects:
+for repo in g.get_user().get_repos():
+    print(repo.name)
 
 print(f"asdf{os.getenv('TOKEN')}asdf")
+print(f"asdf{os.getenv('GITHUB_REPOSITORY')}asdf")
+print(f"asdf{os.getenv('GITHUB_BASE_REF')}asdf")
+print(f"asdf{os.getenv('GITHUB_API_URL')}asdf")
+
+def assign_reviewer(branch: str, reviewer: List[str], github: Github):
+    pass
+
+def assign_reviewee(branch: str, reviewer: List[str], github: Github):
+    pass
