4e4dff39e2ff6726681c567b86a9e7462cb2f051
nhjeon <nakhyun@jobis.co>
2023-09-04 16:50:06 +0900

    test

--- app/main.py
+++ app/main.py
@@ -12,6 +12,8 @@ g = Github(auth=auth)
 g = Github(base_url=f"{os.getenv('GITHUB_API_URL')}", auth=auth)
 
 print(f"{os.getenv('GITHUB_REF')}")
+#refs/pull/<pr_number>/merge
+
 
 repo = g.get_repo(os.getenv('GITHUB_REPOSITORY'), lazy=True)
 
@@ -19,8 +21,13 @@ print(os.getenv('REVIEWERS'))
 
 pulls = repo.get_pulls(state='open')
 for pr in pulls:
-    print(pr.user)
     print(pr.assignees)
+    pr.add_to_assignees(pr.user.name)
+    print(pr)
+    print(pr.assignees)
+    print(pr.assignees)
+    pr.add_to_assignees(pr.user.login)
+    print(pr)
     print(pr.assignees)
 
 def assign_reviewer(branch: str, reviewer: List[str], github: Github):
